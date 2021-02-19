745abbb3cd703cf00ceafb0c2deff1044e584906
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-19 23:12:50 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,18 @@ import Heading from './Heading.js';
 import SubHeading from './SubHeading.js';
 import InputQuery from './InputQuery.js';
 import SubmitButton from './SubmitButton.js';
+import { render } from "enzyme";
 
  const App = () => {
+   render(){
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <h1>{this.Heading()} </h1>
+      <h1> {this.SubHeading()}</h1>
+      <h1>{this.Inputquery()} </h1>
+      <h1>{this.SubmitButton()} </h1>
     </div>
-  )
-}
+)
+}};
 export default App;
 
